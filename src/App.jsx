59417b51abc08fab52a46cb2bfb542e621f720e0
JakubPaszko2001 <jakubpaszko01@gmail.com>
2025-07-08 11:31:54 +0200

    normalny loading sekcji

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
-import React, { Suspense, lazy } from 'react';
 import MainPage from './Components/MainPage';
 import Navbar from './Components/Navbar';
 import Offer from './Components/Offer';
-
-// Lazy-loaded komponenty:
-const Order = lazy(() => import('./Components/Order'));
-const Contact = lazy(() => import('./Components/Contact'));
-const Footer = lazy(() => import('./Components/Footer'));
-
+import Order from './Components/Order';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 function App() {
   return (
-    <div className="font-Main">
+    <div className='font-Main'>
       <Navbar />
       <MainPage />
       <Offer />
-
-      <Suspense fallback={<div className="text-center text-white py-8">Ładowanie formularza...</div>}>
-        <Order />
-      </Suspense>
-
-      <Suspense fallback={<div className="text-center text-white py-8">Ładowanie sekcji kontaktowej...</div>}>
-        <Contact />
-      </Suspense>
-
-      <Suspense fallback={<div className="text-center text-white py-8">Ładowanie stopki...</div>}>
-        <Footer />
-      </Suspense>
+      <Order />
+      <Contact />
+      <Footer />
     </div>
   );
 }
